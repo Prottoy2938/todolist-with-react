0f58168166e203e7c7bb0ae2e6cbd57b8b4ab7c4
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-04 12:21:12 +0600

    Updates

---
+++ precache-manifest.545f9f6e14907d6f8c9d289185db445d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5a4904ee74257a81ecd9ccfbd8424f",
+    "url": "/lights-out/index.html"
+  },
+  {
+    "revision": "212dfd8d4ec6e95749d1",
+    "url": "/lights-out/static/css/main.8eb288e8.chunk.css"
+  },
+  {
+    "revision": "fa4fb7278c6f4873f622",
+    "url": "/lights-out/static/js/2.885e0570.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lights-out/static/js/2.885e0570.chunk.js.LICENSE"
+  },
+  {
+    "revision": "212dfd8d4ec6e95749d1",
+    "url": "/lights-out/static/js/main.1443ab1b.chunk.js"
+  },
+  {
+    "revision": "ffb112e002a919f30cf4",
+    "url": "/lights-out/static/js/runtime-main.d0141581.js"
+  }
+]);
